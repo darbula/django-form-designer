e3d12741a42c49e5512e94b070be35486d6c2fa8
Damir Arbula <damir.arbula@gmail.com>
2016-07-28 08:03:43 +0200

    block mail quick

--- form_designer/views.py
+++ form_designer/views.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from form_designer.forms import DesignedForm
 from form_designer.models import FormDefinition, FormLog
 from form_designer.uploads import handle_uploaded_files
-from form_designer.signals import (designedform_submit, designedform_success, 
+from form_designer.signals import (designedform_submit, designedform_success,
                                 designedform_error, designedform_render)
 
 
@@ -34,6 +34,11 @@ def process_form(request, form_definition, extra_context={}, disable_redirection
         is_submit = True
 
     if is_submit:
+        try:
+            if form.data['Email'] in app_settings.BLOCKED_MAIL:
+                return context
+        except:
+            pass
         designedform_submit.send(sender=process_form, context=context,
             form_definition=form_definition, request=request)
         if form.is_valid():
@@ -77,11 +82,11 @@ def process_form(request, form_definition, extra_context={}, disable_redirection
         'form_definition': form_definition
     })
     context.update(csrf(request))
-    
+
     if form_definition.display_logged:
         logs = form_definition.logs.all().order_by('created')
         context.update({'logs': logs})
-        
+
     return context
 
 def _form_detail_view(request, form_definition):
@@ -96,8 +101,8 @@ def _form_detail_view(request, form_definition):
 
 def detail(request, object_name):
     form_definition = get_object_or_404(FormDefinition, name=object_name, require_hash=False)
-    return _form_detail_view(request, form_definition) 
+    return _form_detail_view(request, form_definition)
 
 def detail_by_hash(request, public_hash):
     form_definition = get_object_or_404(FormDefinition, public_hash=public_hash)
-    return _form_detail_view(request, form_definition) 
+    return _form_detail_view(request, form_definition)
