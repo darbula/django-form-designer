a93cad6b308e268d7131eb3f5644721011dfe059
Damir Arbula <damir.arbula@gmail.com>
2019-04-19 21:40:47 +0200

    django 1.10 compatibility

--- form_designer/urls.py
+++ form_designer/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from form_designer.views import detail, detail_by_hash
 
-urlpatterns = patterns('',
-    url(r'^(?P<object_name>[-\w]+)/$', 'form_designer.views.detail', name='form_designer_detail'),
-    url(r'^h/(?P<public_hash>[-\w]+)/$', 'form_designer.views.detail_by_hash', name='form_designer_detail_by_hash'),
-)
+
+urlpatterns = [
+    url(r'^(?P<object_name>[-\w]+)/$', detail, name='form_designer_detail'),
+    url(r'^h/(?P<public_hash>[-\w]+)/$', detail_by_hash, name='form_designer_detail_by_hash'),
+]
--- form_designer/views.py
+++ form_designer/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.conf import settings
 from form_designer import settings as app_settings
 from django.contrib import messages
-from django.core.context_processors import csrf
+from django.template.context_processors import csrf
 
 import os
 import random
@@ -14,7 +14,7 @@ from datetime import datetime
 from form_designer.forms import DesignedForm
 from form_designer.models import FormDefinition, FormLog
 from form_designer.uploads import handle_uploaded_files
-from form_designer.signals import (designedform_submit, designedform_success, 
+from form_designer.signals import (designedform_submit, designedform_success,
                                 designedform_error, designedform_render)
 
 
@@ -77,11 +77,11 @@ def process_form(request, form_definition, extra_context={}, disable_redirection
         'form_definition': form_definition
     })
     context.update(csrf(request))
-    
+
     if form_definition.display_logged:
         logs = form_definition.logs.all().order_by('created')
         context.update({'logs': logs})
-        
+
     return context
 
 def _form_detail_view(request, form_definition):
@@ -96,8 +96,8 @@ def _form_detail_view(request, form_definition):
 
 def detail(request, object_name):
     form_definition = get_object_or_404(FormDefinition, name=object_name, require_hash=False)
-    return _form_detail_view(request, form_definition) 
+    return _form_detail_view(request, form_definition)
 
 def detail_by_hash(request, public_hash):
     form_definition = get_object_or_404(FormDefinition, public_hash=public_hash)
-    return _form_detail_view(request, form_definition) 
+    return _form_detail_view(request, form_definition)
